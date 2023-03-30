fe67ba28cbc8598cff7a97e0e520ac6729e3a891
yann <yannlefranc330@gmail.com>
2023-03-30 09:47:09 +0200

    JeuxvideoCombots

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //voici l'écriture d'un petit programmes.
+
+        String jeuxvideo1 = "thewitcher "; //String est une variable.
+        String jeuxvideo2 = "tombraider";
+        String combot = jeuxvideo1 + jeuxvideo2; //l'opérateur + permet de combiner toutes cs variables
+        System.out.println(combot); //println permet d'afficher les variables
+
+
+    }
+}
+
+
